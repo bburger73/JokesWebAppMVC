d7b6c7af9fc55745638df780b0ee0193e54fb743
Ben <bburger73@gmail.com>
2023-01-12 16:35:28 -0600

    Add project files.

---
+++ JokesWebAppMVC/Data/JokesWebAppMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using JokesWebAppMVC.Models;
+
+namespace JokesWebAppMVC.Data
+{
+    public class JokesWebAppMVCContext : DbContext
+    {
+        public JokesWebAppMVCContext (DbContextOptions<JokesWebAppMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Joke> Joke { get; set; } = default!;
+    }
+}
